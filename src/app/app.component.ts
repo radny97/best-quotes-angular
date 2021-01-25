1a8836050e008c5bcf56bba664ca5436ef837fbc
radny97 <60014301+radny97@users.noreply.github.com>
2021-01-25 17:15:05 +0100

    commit przed rozpoczęciem komponentów

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/database';
+import { Quotation } from './models/quotation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {author:'',sentence:'',votes:0};
+
+  onSwitchForm(): void{
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation(){
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author:'',sentence:'',votes:0};
+  }
+
+  addVote(quotation: Quotation, value: number){
+    quotation.votes += value;
+  }
+}
