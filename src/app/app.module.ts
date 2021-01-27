3637f37cbd3a76b1c0a7691e531f3ce959033847
radny97 <60014301+radny97@users.noreply.github.com>
2021-01-27 19:05:54 +0100

    nie dokonczyłem czegos z ng-content ale już prawie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RankingComponent,
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
